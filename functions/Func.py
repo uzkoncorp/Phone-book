2f00e35a389140f98c846a4c5ee157c544b6addd
uzkoncorp <sabtim40@gmail.com>
2025-03-24 16:54:07 +0400

    add verify
    Вынесли функции верифицирования в отдельный файл

--- functions/Func.py
+++ functions/Func.py
@@ -1,16 +1,4 @@
-def verify_number(numbers):
-    correct_number = []
-    numbers = numbers.replace(" ", "")
-    numbers = numbers.replace("+7", "8")
-    for i in numbers.split(','):
-        if len(i) == 11:
-            correct_number.append('8' + i[1:])
-    return correct_number
-
-
-def verify_name(name: str) -> str:
-    name = name[0].upper() + name[1:len(name)].lower()
-    return name
+from verify import *
 
 
 
